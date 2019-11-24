0be28d48b454c1e7d86a556d2118be346b9d8004
Victor Cardoso <victor_92santos@hotmail.com>
2019-11-24 17:00:40 -0300

    ajuste de lint

--- src/database/migrations/20191120165432-add-processname-field-to-softwares_profiles.js
+++ src/database/migrations/20191120165432-add-processname-field-to-softwares_profiles.js
@@ -1,16 +1,15 @@
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-      return queryInterface.addColumn('softwares_profiles', 'process_name', {
-        type: Sequelize.INTEGER,
-        references: { model: 'softwares', key: 'process_name' },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: true,
-      });
-    },
-  
-    down: queryInterface => {
-      return queryInterface.removeColumn('softwares_profiles', 'process_name');
-    },
-  };
-  
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('softwares_profiles', 'process_name', {
+      type: Sequelize.INTEGER,
+      references: { model: 'softwares', key: 'process_name' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.removeColumn('softwares_profiles', 'process_name');
+  },
+};
