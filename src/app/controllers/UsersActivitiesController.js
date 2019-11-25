5e22ed37a4602088e566cdb178a0b46295c5ae93
Victor Cardoso <victor_92santos@hotmail.com>
2019-11-24 22:53:10 -0300

    ajuste de lint

--- src/app/controllers/UsersActivitiesController.js
+++ src/app/controllers/UsersActivitiesController.js
@@ -2,32 +2,30 @@ import Sequelize from '../../config/sequelize';
 
 class UsersActivitiesController {
   async index(req, res) {
-
-    console.log(req.body)
-
     const usersActivities = await Sequelize.query(
-      "SELECT username, "+
-      "(SELECT time " +
-      " FROM activities " +
-      " WHERE DATE(time) = DATE " +
-      " ORDER BY time ASC " +
-      "LIMIT 1) AS startDate, " +
-      "(SELECT time " +
-      "  FROM activities " +
-      "  WHERE DATE(time) = DATE " +
-      "  ORDER BY time DESC " +
-      "  LIMIT 1) AS finishDate " +
-      "FROM (SELECT DISTINCT DATE(time) AS DATE, username " +
-      "    FROM activities " +
-      "WHERE " +
-      "   date >= '" + req.body.startDate + "' " +
-      "   and date <= '" + req.body.endDate + "')");
+      `${'SELECT username, ' +
+        '(SELECT time ' +
+        ' FROM activities ' +
+        ' WHERE DATE(time) = DATE ' +
+        ' ORDER BY time ASC ' +
+        'LIMIT 1) AS startDate, ' +
+        '(SELECT time ' +
+        '  FROM activities ' +
+        '  WHERE DATE(time) = DATE ' +
+        '  ORDER BY time DESC ' +
+        '  LIMIT 1) AS finishDate ' +
+        'FROM (SELECT DISTINCT DATE(time) AS DATE, username ' +
+        '    FROM activities ' +
+        'WHERE ' +
+        "   date >= '"}${req.body.startDate}' ` +
+        `   and date <= '${req.body.endDate}')`
+    );
 
     const data = {
-      usersActivities: usersActivities
+      usersActivities,
     };
 
-    return res.status(200).json(data)
+    return res.status(200).json(data);
   }
 }
 
