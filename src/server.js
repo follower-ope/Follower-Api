b9747a35b0fc87818812422a35cac406cea30970
vitor.pereira <vitor.pereira@giganti.com.br>
2019-11-27 13:26:25 -0300

    fix port

--- src/server.js
+++ src/server.js
@@ -1,6 +1,4 @@
 import app from './app';
 //import ProfilesController from './app/controllers/ProfilesController';
 
-app.listen(5000, () => {
-  //ProfilesController.store('administrador');
-});
+app.listen(process.env.PORT || 5000);
